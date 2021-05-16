0b3e9acb0e08c7c0211e775795feb7f9787b6d0e
t-k- <clock126@126.com>
2021-05-16 11:20:32 -0400

    update eval-arqmath-task1.sh

--- eval-arqmath-task1.sh
+++ eval-arqmath-task1.sh
@@ -1,8 +1,12 @@
 EVAL="trec_eval ./topics-and-qrels/qrels.arqmath-2020-task1.txt"
-RUN="${1-tmp.run} -q"
+RUN="${1-tmp.run}"
+
+TMPRUN=$(mktemp)
+echo "creating $TMPRUN" > /dev/stderr
+cat $RUN | awk '{print $1 "\t" "_" "\t" $2 "\t" $3 "\t" $4 "\t" $5}' > $TMPRUN
 
 set -e
-$EVAL $RUN -J -m ndcg
-$EVAL $RUN -J -l2 -m map
-$EVAL $RUN -l2 -m P.10
-$EVAL $RUN -l2 -m bpref
+$EVAL -q $TMPRUN -J -m ndcg
+$EVAL -q $TMPRUN -J -l2 -m map
+$EVAL -q $TMPRUN -l2 -m P.10
+$EVAL -q $TMPRUN -l2 -m bpref
