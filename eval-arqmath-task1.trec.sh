9ea3bab6e61a0ebe45d3587b48b6dc7eed57f5f2
t-k- <clock126@126.com>
2021-05-28 14:08:53 -0400

    two more eval scripts.

---
+++ eval-arqmath-task1.trec.sh
@@ -0,0 +1,9 @@
+EVAL="trec_eval ./topics-and-qrels/qrels.arqmath-2020-task1.txt"
+RUN="${1-tmp.run}"
+
+TMPRUN=${RUN}
+set -e
+$EVAL -q $TMPRUN -J -m ndcg
+$EVAL -q $TMPRUN -J -l2 -m map
+$EVAL -q $TMPRUN -l2 -m P.10
+$EVAL -q $TMPRUN -l2 -m bpref
