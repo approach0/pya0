a23dbd001aab2629db9ec54ae2b013d06bf13b2d
t-k- <clock126@126.com>
2021-09-06 22:38:35 -0400

    allow positive answer by votes.

--- utils/dataset-adapter/arqmath-2021-train-data.py
+++ utils/dataset-adapter/arqmath-2021-train-data.py
@@ -27,14 +27,17 @@ def generate_contrastive_pairs(
     q_dict_file='arqmath-question-dict.pkl',
     a_dict_file='arqmath-answer-dict.pkl',
     tag_bank_file='arqmath-tag-bank.pkl',
+    answer_bank_file='arqmath-answer-bank.pkl',
     n_splits=4, limit=-1, debug=False,
-    random_seed=123):
+    random_seed=123, allow_vote_postive=False):
 
     random.seed(random_seed)
 
     q_dict = load_pickle_file(q_dict_file)
     a_dict = load_pickle_file(a_dict_file)
     tag_bank = load_pickle_file(tag_bank_file)
+    if allow_vote_postive:
+        answer_bank = load_pickle_file(answer_bank_file)
 
     all_questions = q_dict.items()
     if limit >= 0: all_questions = all_questions[:limit]
@@ -46,32 +49,40 @@ def generate_contrastive_pairs(
         for qid, (ac, tags, Q) in progress:
             reminder = aggregate_cnt % n_per_split
             progress.set_description(f'{reminder} % {n_per_split}')
-            if ac not in a_dict:
-                continue
-            positive_A = a_dict[ac]
-            tag = random.choice(tags)
-            negative_id = random.choice(tag_bank[tag])
-            if negative_id not in a_dict:
-                print(f'Warning: neg answer #{negative_id} not found!')
-                continue
-            negative_A = a_dict[negative_id]
-
-            if debug:
-                print('----\n')
-                print(Q, end='\n\n')
-                print(positive_A, end='\n\n')
-                print(f'[{tag}]')
-                print(negative_A, end='\n\n')
-                quit(0)
-
+            # retrieve postive
+            if allow_vote_postive:
+                positives = filter(lambda x: x[1] > 0, answer_bank[qid])
+                positives = [a_dict[p[0]] for p in positives]
+            else:
+                if ac not in a_dict:
+                    continue
+                positives = [a_dict[ac]]
             Q = preprocess_for_transformer(Q)
-            positive_A = preprocess_for_transformer(positive_A)
-            negative_A = preprocess_for_transformer(negative_A)
 
-            aggregate.append((Q, tag, positive_A, negative_A))
-            aggregate_cnt += 1
-            if aggregate_cnt % n_per_split == 0:
-                aggregate = dump_split(aggregate, aggregate_cnt)
+            for positive_A in positives:
+                # sample negative
+                tag = random.choice(tags)
+                negative_id = random.choice(tag_bank[tag])
+                if negative_id not in a_dict:
+                    print(f'Warning: neg answer #{negative_id} not found!')
+                    continue
+                negative_A = a_dict[negative_id]
+
+                if debug:
+                    print('----\n')
+                    print(Q, end='\n\n')
+                    print(positive_A, end='\n\n')
+                    print(f'[{tag}]')
+                    print(negative_A, end='\n\n')
+                    quit(0)
+
+                positive_A = preprocess_for_transformer(positive_A)
+                negative_A = preprocess_for_transformer(negative_A)
+
+                aggregate.append((Q, tag, positive_A, negative_A))
+                aggregate_cnt += 1
+                if aggregate_cnt % n_per_split == 0:
+                    aggregate = dump_split(aggregate, aggregate_cnt)
 
         dump_split(aggregate, aggregate_cnt)
 
--- utils/dataset-adapter/arqmath-2021.py
+++ utils/dataset-adapter/arqmath-2021.py
@@ -68,9 +68,33 @@ def generate_tag_bank(corpus, endat=-1):
         pickle.dump(tag_dict, fh)
 
 
+def generate_answer_bank(corpus, endat=-1):
+    with open('arqmath-question-dict.pkl', 'rb') as fh:
+        Q_dict = pickle.load(fh)
+
+    answer_bank = defaultdict(list)
+    for cnt, dirname, fname in file_iterator(corpus, endat, 'answer'):
+        path = dirname + '/' + fname
+        A = file_read(path)
+        fields = os.path.basename(path).split('.')
+        A_id = int(fields[0])
+        Q_id = int(fields[1])
+
+        if Q_id not in Q_dict:
+            continue
+
+        vote_file = f'{dirname}/{A_id}.answer_vote'
+        upvotes = int(file_read(vote_file))
+        answer_bank[Q_id].append((A_id, upvotes))
+
+    with open('arqmath-answer-bank.pkl', 'wb') as fh:
+        pickle.dump(answer_bank, fh)
+
+
 if __name__ == '__main__':
     os.environ["PAGER"] = 'cat'
     fire.Fire({
         'question_dict': generate_qdict,
         'tag_bank': generate_tag_bank,
+        'answer_bank': generate_answer_bank
     })
