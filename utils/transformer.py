df59012f94fb11f055a85cb8fa06e9afb5e73cf8
t-k- <clock126@126.com>
2021-09-09 15:47:12 -0400

    added attention visualization.

--- utils/transformer.py
+++ utils/transformer.py
@@ -404,6 +404,79 @@ class Trainer(BaseTrainer):
         )
 
 
+def attention_visualize(ckpoint, tok_ckpoint, passage_file):
+    """
+    Visualize attention layers for a given input passage
+    """
+    assert(os.path.isfile(passage_file))
+    import _pya0
+    from preprocess import preprocess_for_transformer
+    import matplotlib.pyplot as plt
+
+    print('Loading model ...')
+    tokenizer = BertTokenizer.from_pretrained(tok_ckpoint)
+    model = BertForPreTraining.from_pretrained(ckpoint,
+        tie_word_embeddings=True,
+        output_attentions=True
+    )
+
+    with open(passage_file, 'r') as fh:
+        passage = fh.read()
+
+    print('Tokenizing ...')
+    passage = preprocess_for_transformer(passage)
+    tokens = tokenizer(passage,
+            padding=True, truncation=True, return_tensors="pt")
+    chars = [tokenizer.decode(c) for c in tokens['input_ids'][0]]
+    chars = [''.join(c.split()).replace('$', '') for c in chars]
+    print(chars, f'length={len(chars)}')
+
+    config = model.config
+    print('bert hiden size:', config.hidden_size)
+    print('bert attention heads:', config.num_attention_heads)
+
+    def hook(l, module, inputs, outputs):
+        multi_head_att_probs = outputs[1]
+        for h in range(config.num_attention_heads):
+            att_probs = multi_head_att_probs[0][h]
+            length = att_probs.shape[0]
+            att_map = att_probs.cpu().detach().numpy()
+            fig, ax = plt.subplots()
+            plt.imshow(att_map, cmap='viridis', interpolation='nearest')
+            plt.yticks(
+                list([i for i in range(length)]),
+                list([chars[i] for i in range(length)])
+            )
+            plt.xticks(
+                list([i for i in range(length)]),
+                list([chars[i] for i in range(length)]),
+                rotation=90
+            )
+            wi, hi = fig.get_size_inches()
+            plt.gcf().set_size_inches(wi * 2, hi * 2)
+            plt.colorbar()
+            #plt.show()
+            save_path = f'./output/layer{l}-head{h}.png'
+            print(f'saving to {save_path}')
+            fig.savefig(save_path)
+            plt.close(fig)
+
+    for l in range(config.num_hidden_layers):
+        layer = model.bert.encoder.layer[l]
+        attention = layer.attention.self
+        partial_hook = partial(hook, l)
+        attention.register_forward_hook(partial_hook)
+
+    device = torch.device(f'cuda:0' if torch.cuda.is_available() else 'cpu')
+    tokens.to(device)
+    model.to(device)
+    model.eval()
+    model(**tokens)
+
+
 if __name__ == '__main__':
     os.environ["PAGER"] = 'cat'
-    fire.Fire(Trainer)
+    fire.Fire({
+        'train': Trainer,
+        'attention': attention_visualize
+    })
