e93da9fa318c2b3d28ec395ead5e14e1fff672dc
t-k- <clock126@126.com>
2021-09-09 22:05:23 -0400

    added utils/tutils.py

---
+++ utils/tutils.py
@@ -0,0 +1,100 @@
+import _pya0
+from preprocess import preprocess_for_transformer
+
+import os
+import fire
+from functools import partial
+
+import torch
+from transformers import BertTokenizer
+from transformers import BertForPreTraining
+
+
+def attention_visualize(ckpoint, tok_ckpoint, passage_file, debug=False):
+    """
+    Visualize attention layers for a given input passage
+    """
+    assert(os.path.isfile(passage_file))
+    import matplotlib.pyplot as plt
+
+    print('Loading model ...')
+    tokenizer = BertTokenizer.from_pretrained(tok_ckpoint)
+    model = BertForPreTraining.from_pretrained(ckpoint,
+        tie_word_embeddings=True,
+        output_attentions=True
+    )
+
+    with open(passage_file, 'r') as fh:
+        passage = fh.read()
+
+    print('Tokenizing ...')
+    passage = preprocess_for_transformer(passage)
+    tokens = tokenizer(passage,
+            padding=True, truncation=True, return_tensors="pt")
+    token_ids = tokens['input_ids'][0]
+    chars = [tokenizer.decode(c) for c in token_ids]
+    chars = [''.join(c.split()).replace('$', '') for c in chars]
+    print(chars, f'length={len(chars)}')
+
+    config = model.config
+    print('bert vocabulary:', config.vocab_size)
+    print('bert hiden size:', config.hidden_size)
+    print('bert attention heads:', config.num_attention_heads)
+
+    def attention_hook(l, module, inputs, outputs):
+        multi_head_att_probs = outputs[1]
+        for h in range(config.num_attention_heads):
+            att_probs = multi_head_att_probs[0][h]
+            length = att_probs.shape[0]
+            att_map = att_probs.cpu().detach().numpy()
+            fig, ax = plt.subplots()
+            plt.imshow(att_map, cmap='viridis', interpolation='nearest')
+            plt.yticks(
+                list([i for i in range(length)]),
+                list([chars[i] for i in range(length)])
+            )
+            plt.xticks(
+                list([i for i in range(length)]),
+                list([chars[i] for i in range(length)]),
+                rotation=90
+            )
+            wi, hi = fig.get_size_inches()
+            plt.gcf().set_size_inches(wi * 2, hi * 2)
+            plt.colorbar()
+            plt.grid(True)
+            save_path = f'./output/layer{l}-head{h}.png'
+            print(f'saving to {save_path}')
+            if debug:
+                plt.show()
+                quit(0)
+            else:
+                fig.savefig(save_path)
+            plt.close(fig)
+
+    for l in range(config.num_hidden_layers):
+        layer = model.bert.encoder.layer[l]
+        attention = layer.attention.self
+        partial_hook = partial(attention_hook, l)
+        attention.register_forward_hook(partial_hook)
+
+    device = torch.device(f'cuda:0' if torch.cuda.is_available() else 'cpu')
+    tokens.to(device)
+    model.to(device)
+    model.eval()
+    model(**tokens)
+
+
+def pft_file(passage_file):
+    with open(passage_file, 'r') as fh:
+        for line in fh:
+            line = line.rstrip()
+            line = preprocess_for_transformer(line)
+            print(line)
+
+
+if __name__ == '__main__':
+    os.environ["PAGER"] = 'cat'
+    fire.Fire({
+        "pft": pft_file,
+        "attention": attention_visualize,
+    })
