f5a5ab18003b40cebf95eb6afea026a125c64bb1
t-k- <clock126@126.com>
2021-09-16 17:18:19 -0400

    bump up version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def copy_files():
 
 setup(
     name = 'pya0',
-    version = '0.3.0',
+    version = '0.3.1',
     author = 'Wei Zhong',
     author_email = "clock126@126.com",
     description = 'Approach Zero Python Interface',
