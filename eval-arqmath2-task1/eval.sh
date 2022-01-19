6f2b144f813222f65f6960ef7a92ad71924084ca
t-k- <clock126@126.com>
2022-01-19 14:33:57 -0500

    fixup and tested against official results.

--- eval-arqmath2-task1/eval.sh
+++ eval-arqmath2-task1/eval.sh
@@ -1,10 +1,25 @@
 DIR=$(dirname $0)
 INPUT=${1-tmp.run}
-default_qrels='topics-and-qrels/qrels.arqmath-2021-task1.txt'
+default_qrels='topics-and-qrels/qrels.arqmath-2021-task1-official.txt'
 QREL=${2-$default_qrels}
 
+mkdir -p $DIR/prime-output
+rm -f $DIR/prime-output/*
+mkdir -p $DIR/trec-output
+rm -f $DIR/trec-output/*
 mkdir -p $DIR/input
-cat $INPUT | awk '{print $1 "\t" $3 "\t" $4 "\t" $5 "\t" $6}' > $DIR/input/$(basename $INPUT)
+
+n_fields=$(awk '{print NF; exit}' $INPUT)
+if [[ $n_fields -eq 6 ]]; then
+    echo "TREC format, we will need to drop the second column..."
+    cat $INPUT | awk '{print $1 "\t" $3 "\t" $4 "\t" $5 "\t" $6}' > $DIR/input/$(basename $INPUT)
+elif [[ $n_fields -eq 5 ]]; then
+    echo "ARQMath-v2 format, no change."
+    cp $INPUT $DIR/input/
+else
+    echo "Unknown format, abort."
+    exit 1
+fi
 sed -i 's/ /\t/g' $DIR/input/*
 
 set -x
