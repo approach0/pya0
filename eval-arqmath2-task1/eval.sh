5ba1927358dac06dd1607f0a01d72cffeb32ec0f
t-k- <clock126@126.com>
2022-01-22 16:57:22 -0500

    Google spreadsheets friendly

--- eval-arqmath2-task1/eval.sh
+++ eval-arqmath2-task1/eval.sh
@@ -15,4 +15,4 @@ sed -i 's/ /\t/g' $DIR/input/*
 python3 $DIR/arqmath_to_prim_task1.py -qre $QREL  -sub "$DIR/input/" -tre $DIR/trec-output/ -pri $DIR/prime-output/
 python3 $DIR/task1_get_results.py -eva "trec_eval" -qre $QREL -pri $DIR/prime-output/ -res "$DIR/result.tsv"
 
-cat $DIR/result.tsv
+cat $DIR/result.tsv | sed -e 's/[[:blank:]]/ /g'
