cc08a831dc96cd09e88c31bd59952c764bceebfa
t-k- <clock126@126.com>
2022-03-11 14:26:43 -0500

    eval-arqmath2-task1: Added --nojudge option.

--- eval-arqmath2-task1/eval.sh
+++ eval-arqmath2-task1/eval.sh
@@ -1,6 +1,24 @@
 DIR=$(dirname $0)
-default_qrels='topics-and-qrels/qrels.arqmath-2021-task1-official.txt'
-QREL=${1-$default_qrels}
+QREL='topics-and-qrels/qrels.arqmath-2021-task1-official.txt'
+
+for arg in "$@"; do
+    case $arg in
+    --nojudge)
+      NOJUDGE=-nojudge
+      shift
+      ;;
+    --qrels=*)
+      QREL="${arg#*=}"
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $arg"
+      exit 1
+      ;;
+    *)
+      ;;
+    esac
+done
 
 mkdir -p $DIR/prime-output
 rm -f $DIR/prime-output/*
@@ -13,6 +31,6 @@ set -x
 sed -i 's/ /\t/g' $DIR/input/*
 
 python3 $DIR/arqmath_to_prim_task1.py -qre $QREL  -sub "$DIR/input/" -tre $DIR/trec-output/ -pri $DIR/prime-output/
-python3 $DIR/task1_get_results.py -eva "trec_eval" -qre $QREL -pri $DIR/prime-output/ -res "$DIR/result.tsv"
+python3 $DIR/task1_get_results.py -eva "trec_eval" -qre $QREL -pri $DIR/prime-output/ -res "$DIR/result.tsv" $NOJUDGE
 
 cat $DIR/result.tsv | sed -e 's/[[:blank:]]/ /g'
