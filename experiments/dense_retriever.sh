f208176842720e5bed336b19d40890e46951f92f
t-k- <clock126@126.com>
2022-03-16 13:21:51 -0400

    fixed RERANK commands and added minor comments.

--- experiments/dense_retriever.sh
+++ experiments/dense_retriever.sh
@@ -1,5 +1,6 @@
-set -x
+set -ex
 
+# Indexing
 INDEX='python -m pya0.transformer_eval index ./utils/transformer_eval.ini'
 
 $INDEX index_ntcir12_dpr --device titan_rtx
@@ -16,6 +17,7 @@ $INDEX index_arqmath2_dpr__scibert_1ep --device a6000_1
 $INDEX index_arqmath2_dpr__vanilla_1ep --device a6000_1
 $INDEX index_arqmath2_colbert --device a6000_1
 
+# Searching
 SEARCH='python -m pya0.transformer_eval search ./utils/transformer_eval.ini'
 
 $SEARCH search_ntcir12_dpr --device cpu
@@ -32,14 +34,16 @@ $SEARCH search_arqmath2_dpr__scibert_1ep --device cpu
 $SEARCH search_arqmath2_dpr__vanilla_1ep --device cpu
 $SEARCH search_arqmath2_colbert --device a6000_1
 
+# Reranking
 RERANK='python -m pya0.transformer_eval maprun ./utils/transformer_eval.ini'
+baseline_run=./runs/arqmath2-a0-task1.run
 
 $RERANK maprun_arqmath2_to_dpr $baseline_run --device a6000_1
 $RERANK maprun_arqmath2_to_colbert $baseline_run --device a6000_1
 
+# fuse baseline with DPR or ColBERT and do kfold cross evaluation
 kfold=5
 kfold_dir=runs.kfold
-baseline_run=./runs/arqmath2-a0-task1.run
 fusion_list=(
     ./runs/search_arqmath2_colbert.run
     ./runs/search_arqmath2_dpr.run
