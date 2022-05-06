a99f45fec9e70e457acd57625d181da1cf75932a
t-k- <clock126@126.com>
2022-05-06 17:06:34 -0400

    added arqmath3-task3 scripts.

---
+++ experiments/arqmath3-task3/map_task1run_to_bestvote_answers.py
@@ -0,0 +1,81 @@
+import pickle
+import argparse
+from pya0.mergerun import parse_trec_file
+
+def TREC_output(run_dict, run_name, output_file="tmp.run"):
+    with open(output_file, 'w') as fh:
+        for qid in run_dict:
+            print("%s %s %s %u %f %s" % (
+                qid,
+                run_dict[qid][0]["_"],
+                run_dict[qid][0]["docid"],
+                "1",
+                #run_dict[qid][0]["rank"],
+                run_dict[qid][0]['score'],
+                run_name
+            ), file=fh)
+            fh.flush()
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run_path', type=str, help='runfile for task 1', required=True)
+    parser.add_argument('--accept_dict', type=str, help='the pickle file maps answers to the best answer under the same question', default="aid2accept_aid.pkl")
+    parser.add_argument('--best_dict', type=str, help='the pickle file maps answers to the accepted under the same question', default="aid2bestgt1_aid.pkl")# default="aid2best_aid.pkl")
+    parser.add_argument('--output_path', type=str, help='path to save the new run file, same format as task1 output', required=True)
+    args = parser.parse_args()
+
+    #if not args.map_dict:
+    #    aid2best = {}
+    #else:
+    #    aid2best = pickle.load(open(args.map_dict, 'rb'))
+    aid2accept = pickle.load(open(args.accept_dict, 'rb'))
+    aid2best = pickle.load(open(args.best_dict, 'rb'))
+
+    output_file = open(args.output_path, 'w')
+
+    run_dict, run_name = parse_trec_file(args.run_path)
+    new_run_dict = {}
+    for qid in run_dict:
+        best_aid = 0
+        best_score = 0
+        sorted_answers = sorted(run_dict[qid], key=lambda x: float(x["score"]), reverse=True)
+        got_answer = False
+
+        for a in sorted_answers:
+            docid = a["docid"]
+            if docid in aid2accept and aid2accept[docid]:
+                new_id = aid2accept[docid]
+                new_run_dict[qid] = [{
+                    "docid": new_id,
+                    "_": "accept",
+                    "rank": a["rank"],
+                    "score": a["score"]
+                }]
+                got_answer = True
+                break
+            if docid in aid2best and aid2best[docid]:
+                new_id = aid2best[docid]
+                new_run_dict[qid] = [{
+                    "docid": new_id,
+                    "_": "best",
+                    "rank": a["rank"],
+                    "score": a["score"],
+                    }]
+                got_answer = True
+                break
+        if not got_answer:
+            new_run_dict[qid] = [{
+                "docid":sorted_answers[0]["docid"],
+                "_": "origin",
+                "rank": sorted_answers[0]["rank"],
+                "score": sorted_answers[0]["score"]
+            }]
+
+    TREC_output(new_run_dict, run_name, output_file=args.output_path)
+
+
+
+
+
+
---
+++ experiments/arqmath3-task3/task3_sentence_filtering.py
@@ -0,0 +1,86 @@
+import argparse
+from nltk.tokenize import sent_tokenize
+
+leng_threshold = 1195
+
+def parse_run(filename):
+    f = open(filename, 'r')
+    query2sentences = {}
+    for line in f:
+        qid, rank, score, rid, sources, answer = line.strip().split("\t")
+        answer = answer.replace('"', "'")
+        answer = '"{}"'.format(answer[1:-1])
+        if len(answer) > leng_threshold or len(answer) == 0:
+            continue
+        if qid not in query2sentences:
+            query2sentences[qid] = [["1", score, rid, sources, answer]]
+        else:
+            query2sentences[qid].append(["1", score, rid, sources, answer])
+
+    return query2sentences, rid
+
+#def chunk_sent(sent, threshold=leng_threshold):
+#    sentences = sent_tokenize(sent)
+#    ret = ""
+#    for s in sentences:
+#        if len(ret + " " + s) < threshold:
+#            ret = ret + " " + s
+#        else:
+#            return ret
+#    if len(ret):
+#        return ret
+#    else:
+#        print(sent)
+#        return sent[:leng_threshold]
+
+def select_highest_score(results, default_rid="maprun-task3.run"):
+    selected_results = {}
+    for qid in results:
+        #current_best = ("1", "0", default_rid, ("", "0", "0"), "") # rank, score, rid, scource, answer
+        current_best = results[qid][0]
+        for sentence in results[qid]:
+            if float(sentence[1]) > float(current_best[1]) and len(sentence[-1]) < leng_threshold:
+                current_best = sentence
+        selected_results[qid] = current_best
+        selected_results[qid][0] = "1"
+#        selected_results[qid][-1] = chunk_sent(selected_results[qid][-1])
+    return selected_results
+
+def select_highest_post_longest(results, default_rid="maprun-task3.run"):
+    selected_results = {}
+    for qid in results:
+        sorted_answers = sorted(results[qid], key=lambda x: float(x[1]), reverse=True)
+        docid = sorted_answers[0][3][0]
+        current_best = sorted_answers[0]
+        for a in sorted_answers:
+            if a[3] == docid:
+                if len(a[-1]) < leng_threshold and len(a[-1]) > len(current_best[-1]):
+                    current_best = a
+        selected_results[qid] = current_best
+        selected_results[qid][0] = "1"
+#        selected_results[qid][-1] = chunk_sent(current_best[-1])
+    return selected_results
+
+def save_results(results, out_filename):
+    f = open(out_filename,'w')
+    for qid in results:
+        f.write('{}\t{}\t{}\n'.format(qid, "\t".join(results[qid][:-1]), results[qid][-1]))
+    f.close() 
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run_path', type=str, help='runfile for task 1', required=True)
+    parser.add_argument('--output_path', type=str, required=True)
+    parser.add_argument('--mode', type=str, help='choose from: highest_score, highest_post_longest')
+    args = parser.parse_args()
+
+    results, rid = parse_run(args.run_path)
+
+    if args.mode == "highest_score":
+        results = select_highest_score(results, rid)
+
+    elif args.mode == "highest_post_longest":
+        results = select_highest_post_longest(results, rid)
+
+    save_results(results, args.output_path)
