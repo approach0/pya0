2d7855e68546edb7fe36d00c3fa39a851fde85e4
t-k- <clock126@126.com>
2022-05-16 16:50:36 -0400

    update experiments/arqmath-prev-years.sh

--- experiments/arqmath-prev-years.sh
+++ experiments/arqmath-prev-years.sh
@@ -1,4 +1,7 @@
 set -xe
+####################
+#  Baseline
+####################
 
 # # ARQMath1
 # RUN=arqmath1-task1-nostemmer.run; > $RUN; python -m pya0 --index /tuna1/scratch/w32zhong/mnt-index-arqmath3_task1_default.img --trec-output $RUN --collection arqmath-2020-task1
@@ -14,12 +17,19 @@ set -xe
 # 
 # RUN=arqmath2-task2.run; > $RUN; python -m pya0 --index /tuna1/scratch/w32zhong/mnt-index-arqmath3_task2_v3.img --trec-output $RUN --collection arqmath-2021-task2-refined
 
-# ARQMath1 ColBERT
+####################
+#  Dense retrieval
+####################
 SEARCH='python -m pya0.transformer_eval search ./utils/transformer_eval.ini'
 MAPRUN='python -m pya0.transformer_eval maprun ./utils/transformer_eval.ini'
+STORE="$(cat ./utils/transformer_eval.ini | grep -Po '(?<=store =).*')"
 DEV=a6000_5
+RUNS="$(echo $STORE)/experiments/runs"
+ls $RUNS
+
+# ARQMath1 ColBERT
 $SEARCH search_arqmath1_colbert --device $DEV
-$MAPRUN maprun_arqmath1_to_colbert --device $DEV
+$MAPRUN maprun_arqmath1_to_colbert $RUNS/search_arqmath1_colbert.run --device $DEV
 
 $SEARCH search_arqmath1_task2_colbert --device $DEV
 
@@ -29,7 +39,7 @@ $SEARCH search_arqmath1_task2_colbert_context_02 --device $DEV
 
 # ARQMath2 ColBERT
 $SEARCH search_arqmath2_colbert --device $DEV
-$MAPRUN maprun_arqmath2_to_colbert --device $DEV
+$MAPRUN maprun_arqmath2_to_colbert $RUNS/search_arqmath2_colbert.run --device $DEV
 
 $SEARCH search_arqmath2_task2_colbert --device $DEV
 
