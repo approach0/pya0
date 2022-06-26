c15949c4b4e7384d2fc198945eac68ed056441d4
t-k- <clock126@126.com>
2022-06-26 13:19:23 -0400

    fix eval-arqmath1-task1.sh to support space in file names.

--- eval-arqmath1-task1.sh
+++ eval-arqmath1-task1.sh
@@ -1,31 +1,30 @@
 set -e
 QREL='topics-and-qrels/qrels.arqmath-2020-task1.txt'
-RUNS="${@-tmp.run}"
+RUNS=("${@-tmp.run}")
 
 trec_eval_for_arqmath() {
-	trec_eval $QREL $1 $2 -J -m ndcg     # NDCG prime
-	trec_eval $QREL $1 $2 -J -l2 -m map  # MAP prime
-	trec_eval $QREL $1 $2 -J -l2 -m P.10 # P@10 prime
-	trec_eval $QREL $1 $2 -l2 -m bpref   # Bpref
-	python -m pya0.judge_rate $QREL $1
+    trec_eval $QREL $1 $2 -J -m ndcg     # NDCG prime
+    trec_eval $QREL $1 $2 -J -l2 -m map  # MAP prime
+    trec_eval $QREL $1 $2 -J -l2 -m P.10 # P@10 prime
+    trec_eval $QREL $1 $2 -l2 -m bpref   # Bpref
+    #python -m pya0.judge_rate $QREL $1
 }
 
-for RUN in $RUNS; do
-	echo $RUN
-
-	TMP=`mktemp`
-    n_fields=$(awk '{print NF; exit}' $RUN)
+for RUN in "${RUNS[@]}"; do
+    TMP=`mktemp`
+    n_fields=$(awk '{print NF; exit}' "$RUN")
     if [[ $n_fields -eq 6 ]]; then
         echo "TREC format, no change..."
-        cp $RUN $TMP
+        cp "$RUN" $TMP
     elif [[ $n_fields -eq 5 ]]; then
         echo "ARQMath-v2 format, insert a second column."
-        cat $RUN | awk '{print $1 "\t" "_" "\t" $2 "\t" $3 "\t" $4 "\t" $5}' > $TMP
+        cat "$RUN" | awk '{print $1 "\t" "_" "\t" $2 "\t" $3 "\t" $4 "\t" $5}' > $TMP
     else
         echo "Unknown format, abort."
         exit 1
     fi
 
-	trec_eval_for_arqmath $TMP
-	#trec_eval_for_arqmath $TMP -q
+    echo $RUN
+    trec_eval_for_arqmath $TMP
+    #trec_eval_for_arqmath $TMP -q
 done
