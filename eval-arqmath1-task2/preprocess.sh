303462298fea7e743875424c19e627ea9bbe2fe8
t-k- <clock126@126.com>
2022-06-27 11:51:17 -0400

    update eval-arqmath1-task2.

--- eval-arqmath1-task2/preprocess.sh
+++ eval-arqmath1-task2/preprocess.sh
@@ -1,5 +1,5 @@
 DIR=$(dirname $0)
-INPUTS=$@
+INPUTS=("${@-tmp.run}")
 mkdir -p $DIR/input
 
 if [ "$INPUTS" == "cleanup" ]; then
@@ -8,21 +8,21 @@ if [ "$INPUTS" == "cleanup" ]; then
     exit 0
 fi
 
-for INPUT in $INPUTS; do
+for INPUT in "${INPUTS[@]}"; do
     if [[ "$INPUT" == "swap" ]]; then
         continue
     fi
 
     echo $INPUT
-    n_fields=$(awk '{print NF; exit}' $INPUT)
-    dest_name=$(basename $INPUT)
+    n_fields=$(awk '{print NF; exit}' "$INPUT")
+    dest_name=$(basename "$INPUT")
     dest_name=$(echo $dest_name | sed -e 's/\./_/g')
     if [[ " $INPUTS " =~ " swap " ]]; then
         echo "SWAP format: Query_Id {Post_Id <=> Formula_Id} Rank Score Run"
-        cat $($DIR/swap-col-2-and-3.sh $INPUT) > $DIR/input/$dest_name
+        cat $($DIR/swap-col-2-and-3.sh "$INPUT") > $DIR/input/$dest_name
     elif [[ $n_fields -eq 6 ]]; then
         echo "ARQMath task2 format: Query_Id Formula_Id Post_Id Rank Score Run"
-        cat $INPUT > $DIR/input/$dest_name
+        cat "$INPUT" > $DIR/input/$dest_name
     else
         echo "Unknown format, abort."
         exit 1
