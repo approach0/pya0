ddb8e03487442dfe711ddde0c6fc8dd3c12da6b5
t-k- <clock126@126.com>
2022-08-25 00:17:42 -0400

    minor change.

--- scripts/effectiveness_ttest.py
+++ scripts/effectiveness_ttest.py
@@ -3,14 +3,14 @@ import fire
 import pandas as pd
 from scipy.stats import ttest_ind, ttest_rel
 
-def do_ttest(*paths, metric='ndcg', baseline_idx=0, model_idx=1):
+def do_ttest(*paths, baseline_idx=0, model_idx=1):
     compare = []
     for path in paths:
         df = pd.read_csv(path, header=None, sep='\t', usecols=[1, 2])
         mean = df.mean().to_numpy()[0]
         std = df.std().to_numpy()[0]
         col = df.to_numpy()[:,1]
-        print(path, metric, 'mean/std:', mean, std)
+        print(path, 'mean/std:', mean, std)
         compare.append(col)
     baseline_col = compare[baseline_idx]
     model_col = compare[model_idx]
