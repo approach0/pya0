68e49c2c41e5ebe025b2c1e05505f3849308c67a
t-k- <clock126@126.com>
2022-08-24 23:37:47 -0400

    update eval-ntcir12.sh

--- eval-ntcir12.sh
+++ eval-ntcir12.sh
@@ -1,21 +1,31 @@
 QREL=./topics-and-qrels/qrels.ntcir12-math-browsing.txt
 EVAL="trec_eval $QREL"
 RUN="${1-tmp.run}"
+TSV_OUTPUT="${2-false}"
 
-set -e
-echo "Fully relevant:"
-$EVAL $RUN -l3 -m P.5
-$EVAL $RUN -l3 -m P.10
-$EVAL $RUN -l3 -m P.15
-$EVAL $RUN -l3 -m P.20
-$EVAL $RUN -l3 -m bpref
+set -ex
+if [ $TSV_OUTPUT == "tsv" ]; then
+    echo -n "$RUN "
+    $EVAL $RUN -l3 -m bpref | awk '{printf $3 " "}'
+else
+    echo "Fully relevant:"
+    $EVAL $RUN -l3 -m P.5
+    $EVAL $RUN -l3 -m P.10
+    $EVAL $RUN -l3 -m P.15
+    $EVAL $RUN -l3 -m P.20
+    $EVAL $RUN -l3 -m bpref
+fi
 
-echo "Partial relevant:"
-$EVAL $RUN -l1 -m P.5
-$EVAL $RUN -l1 -m P.10
-$EVAL $RUN -l1 -m P.15
-$EVAL $RUN -l1 -m P.20
-$EVAL $RUN -l1 -m bpref
+if [ $TSV_OUTPUT == "tsv" ]; then
+    $EVAL $RUN -l1 -m bpref | awk '{print $3}'
+else
+    echo "Partial relevant:"
+    $EVAL $RUN -l1 -m P.5
+    $EVAL $RUN -l1 -m P.10
+    $EVAL $RUN -l1 -m P.15
+    $EVAL $RUN -l1 -m P.20
+    $EVAL $RUN -l1 -m bpref
+fi
 
 if [ -e pya0/judge_rate.py ]; then
     echo -n "Judge Rate: "
