734d3b13f0a8d615c24d2bd3ec118c08f68b4e1b
t-k- <clock126@126.com>
2022-08-29 14:13:34 -0400

    added Condenser/MAE loss compute.

--- utils/condenser.py
+++ utils/condenser.py
@@ -1,11 +1,16 @@
 import os
 import json
 import torch
+from collections import namedtuple
 from torch import nn
 from transformers import BertLayer
-from transformers.models.bert.modeling_bert import BertOnlyMLMHead
+from transformers.models.bert.modeling_bert import BertPreTrainingHeads, BertPooler
 from transformers import BertTokenizer
 from transformers import BertForPreTraining
+
+
+outputs = lambda d: namedtuple('outputs', d.keys())(**d)
+
 
 class Condenser(nn.Module):
     def __init__(self, n_dec_layers=2, skip_from=0):
@@ -21,17 +26,20 @@ class Condenser(nn.Module):
         self.dec = nn.ModuleList(
             [BertLayer(config) for _ in range(n_dec_layers)]
         )
-        self.dec_mlm_head = BertOnlyMLMHead(config)
+        self.dec_pretrain_head = BertPreTrainingHeads(config)
+        self.dec_pretrain_pooler = BertPooler(config)
 
         # load as much as good initial weights
         self.dec.apply(self.enc._init_weights)
-        self.dec_mlm_head.apply(self.enc._init_weights)
+        self.dec_pretrain_head.apply(self.enc._init_weights)
+        self.dec_pretrain_pooler.apply(self.enc._init_weights)
 
         # save parameter
         self.n_dec_layers = n_dec_layers
         self.skip_from = skip_from
 
-    def forward(self, inputs, mode, cot_cls_hiddens=None):
+    def forward(self, inputs, mode,
+        labels=None, next_sentence_label=None, cot_cls_hiddens=None):
         assert mode in ['condenser', 'cot-mae-enc', 'cot-mae-dec']
 
         enc_output = self.enc(
@@ -43,16 +51,31 @@ class Condenser(nn.Module):
 
         # all_hidden_states == all_hidden_states + (hidden_states,)
         enc_hidden_states = enc_output.hidden_states # [13, B, N, 768]
+        enc_output_preds = enc_output.prediction_logits # [B, N, vocab_size]
         # where B is batch size and N is the sequence length.
-        # the enc_hidden_states contains a 13-element tuple where
+        # MLM logits unused positions (e.g., CLS) are ignored by CE_IGN_IDX,
+        # and the enc_hidden_states contains a 13-element tuple where
         # the 1st one is the initial input embeddings.
+
         cls_hiddens = enc_hidden_states[-1][:, :1]
         skip_hiddens = enc_hidden_states[self.skip_from][:, 1:]
+        #print(enc_hidden_states[-1].shape) # [B, N, 768]
         #print(cls_hiddens.shape)  # [B, 1, 768]
         #print(skip_hiddens.shape) # [B, N-1, 768]
 
         if mode == 'cot-mae-enc':
-            return enc_output.prediction_logits, cls_hiddens
+            if labels is not None:
+                loss_func = nn.CrossEntropyLoss()
+                vocab_size = self.enc.config.vocab_size
+                # MAE encoder loss
+                enc_mlm_loss = loss_func(enc_output_preds.view(-1, vocab_size), labels.view(-1))
+            else:
+                enc_mlm_loss = None
+            return outputs({
+                'enc_output_preds': enc_output_preds,
+                'cls_hiddens': cls_hiddens,
+                'loss': enc_mlm_loss
+            })
         elif mode == 'cot-mae-dec':
             hiddens = torch.cat([cot_cls_hiddens, skip_hiddens], dim=1)
         elif mode == 'condenser':
@@ -73,8 +96,30 @@ class Condenser(nn.Module):
             # layer_out == (layer_out,) + attention_weights
             hiddens = layer_out[0]
 
-        dec_output_preds = self.dec_mlm_head(hiddens)
-        return enc_output.prediction_logits, dec_output_preds
+        sequence_output = hiddens
+        pooled_output = self.dec_pretrain_pooler(sequence_output)
+        dec_output_preds, dec_ctx_preds = self.dec_pretrain_head(sequence_output, pooled_output)
+        #print(dec_output_preds.shape) # [B, N, vocab_size]
+        #print(dec_ctx_preds.shape) # [B, 2]
+
+        if labels is not None and next_sentence_label is not None:
+            loss_func = nn.CrossEntropyLoss()
+            vocab_size = self.enc.config.vocab_size
+            dec_mlm_loss = loss_func(dec_output_preds.view(-1, vocab_size), labels.view(-1))
+            dec_ctx_loss = loss_func(dec_ctx_preds.view(-1, 2), next_sentence_label.view(-1))
+            if mode == 'condenser':
+                enc_mlm_loss = loss_func(enc_output_preds.view(-1, vocab_size), labels.view(-1))
+                loss = enc_mlm_loss + dec_mlm_loss + dec_ctx_loss # CoCondenser loss
+            else:
+                loss = dec_mlm_loss + dec_ctx_loss # MAE decoder loss
+        else:
+            loss = None
+        return outputs({
+            'enc_output_preds': enc_output_preds,
+            'dec_output_preds': dec_output_preds,
+            'dec_ctx_preds': dec_ctx_preds,
+            'loss': loss
+        })
 
     def save_pretrained(self, path):
         self.enc.save_pretrained(os.path.join(path, 'encoder.ckpt'))
@@ -108,9 +153,9 @@ if __name__ == '__main__':
     inputs = tokenizer('foo bar', truncation=True, return_tensors="pt")
 
     condenser = Condenser()
-    enc_output, dec_output = condenser(inputs, 'condenser')
-    print(enc_output.shape)
-    print(dec_output.shape)
+    outputs = condenser(inputs, 'condenser')
+    print(outputs.enc_output_preds.shape)
+    print(outputs.dec_output_preds.shape)
 
     condenser.save_pretrained('./test-condenser')
     condenser_ckpt = Condenser.from_pretrained('./test-condenser')
