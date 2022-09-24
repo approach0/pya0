ac6ea88ff3cdb4f421d364be5e2541d421de59bb
t-k- <clock126@126.com>
2022-09-24 13:45:30 -0400

    added replace_isolated_groups option in mse_aops_dataloader()

--- utils/dataset-adapter/mse-aops-2021.py
+++ utils/dataset-adapter/mse-aops-2021.py
@@ -33,7 +33,23 @@ def mse_aops_json_file_iterator(corpus, endat):
                 break
 
 
-def mse_aops_dataloader(corpus, endat=0, num_tokenizer_ver=1):
+def unwrap_isolated_tex_group(text, group_name):
+    regex = re.compile(
+        r"\\begin{" + group_name +
+        r"\*?}(.+?)\\end{" + group_name +
+        r"\*?}(?!\s+\[/imath\])", re.DOTALL) # negative lookahead
+    return re.sub(regex, r"[imath]\1[/imath]", text)
+
+
+def unwrap_isolated_tex_groups(text,
+    groups=['align', 'alignat', 'equation', 'gather']):
+    for grp in groups:
+        text = unwrap_isolated_tex_group(text, grp)
+    return text
+
+
+def mse_aops_dataloader(corpus, endat=0, num_tokenizer_ver=1,
+    replace_isolated_groups=True):
     dataset = []
     vocab = defaultdict(int)
     word_tokenizer = RegexpTokenizer(r'\w+')
@@ -44,6 +60,8 @@ def mse_aops_dataloader(corpus, endat=0, num_tokenizer_ver=1):
         text = j['text']
         tags = j['tags'] if 'tags' in j else ''
         url = j['url']
+        if replace_isolated_groups:
+            text = unwrap_isolated_tex_groups(text)
         document = preprocess.preprocess_for_transformer(text, vocab,
             num_tokenizer_ver=num_tokenizer_ver)
         sentences = sent_tokenize(document)
