421bde54d5263b133138b9c974d42243073b43df
t-k- <clock126@126.com>
2022-12-06 10:32:02 -0500

    added experiments/compare_ckpts_in_different_eps.sh

---
+++ experiments/compare_ckpts_in_different_eps.sh
@@ -0,0 +1,6 @@
+export PYTHONPATH="$(cd .. && pwd)"
+for model in bertnsp cocomae cotbert; do
+	python -m pya0.transformer_utils eval_trained_ckpts inference.ini pipeline__eval_arqmath3_single_vec --rounded_ep False \
+		./math-tokenizer/ a6000_0:32 models/job-single_vec_retriever-a6000-using-${model}-single_vec_retriever
+	mv eval_trained_ckpts.pkl eval_trained_ckpts--${model}-single_vec_retriever.pkl
+done
