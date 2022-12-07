3ff2538edf8aaa1ece991ab08f42783dc203b5ae
t-k- <clock126@126.com>
2022-12-07 12:35:36 -0500

    update training-and-inference/prepare.sh

--- training-and-inference/prepare.sh
+++ training-and-inference/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 export PYTHONPATH="$(cd .. && pwd)"
-ARQMathVER=V1.2
+ARQMathVER=V1.3
 
 wget https://vault.cs.uwaterloo.ca/s/8ipWsPbPMQ3qFZS/download -O mse-aops-2021.tar.gz
 tar xzf mse-aops-2021.tar.gz
@@ -20,8 +20,8 @@ python -m pya0.mse-aops-2021-train-data generate_sentpairs \
 
 (cd data.pretrain-bertnsp && ls | tee shards.txt)
 (cd data.pretrain-cotmae && ls | tee shards.txt)
-python -m pya0.transformer_utils pft_print ../tests/transformer_unmask.txt > data.pretrain-bertnsp/test.txt
-python -m pya0.transformer_utils pft_print ../tests/transformer_unmask.txt > data.pretrain-cotmae/test.txt
+python -m pya0.transformer_utils unmask_input_print ../tests/transformer_unmask.txt > data.pretrain-bertnsp/test.txt
+python -m pya0.transformer_utils unmask_input_print ../tests/transformer_unmask.txt > data.pretrain-cotmae/test.txt
 
 wget https://vault.cs.uwaterloo.ca/s/rdRkP4ZYRqLjgiS/download -O ./datasets/Posts.${ARQMathVER}.xml
 python -m pya0.arqmath-2021 gen_question_dict ./datasets/Posts.${ARQMathVER}.xml
