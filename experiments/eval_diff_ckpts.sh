469e3f7f1e7e626ff97e86caab72f2367f00f874
t-k- <clock126@126.com>
2022-12-08 22:52:48 -0500

    update experiments/eval_diff_ckpts.sh

--- experiments/eval_diff_ckpts.sh
+++ experiments/eval_diff_ckpts.sh
@@ -1,6 +1,8 @@
 export PYTHONPATH="$(cd .. && pwd)"
-for bkb in bertnsp cotbert cocondenser cocomae; do
-	python -m pya0.transformer_eval index inference.ini index_arqmath3_single_vec --device a6000_0:32 --backbone ${bkb} --ckpt 0-2-0
-	python -m pya0.transformer_eval index inference.ini index_arqmath3_single_vec --device a6000_0:32 --backbone ${bkb} --ckpt 1-2-0
-	python -m pya0.transformer_eval index inference.ini index_arqmath3_single_vec --device a6000_0:32 --backbone ${bkb} --ckpt 3-2-0
+GPU=a6000_7:48
+#for bkb in bertnsp cotbert cocondenser cocomae; do
+for bkb in cocondenser cocomae; do
+	python -m pya0.transformer_eval index inference.ini index_arqmath3_single_vec --device $GPU --backbone ${bkb} --ckpt 0-2-0
+	python -m pya0.transformer_eval index inference.ini index_arqmath3_single_vec --device $GPU --backbone ${bkb} --ckpt 1-2-0
+	python -m pya0.transformer_eval index inference.ini index_arqmath3_single_vec --device $GPU --backbone ${bkb} --ckpt 3-2-0
 done
