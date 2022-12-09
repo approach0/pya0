4e461efeea42d5ece92381d0bc3cf6a2e83f5a04
t-k- <clock126@126.com>
2022-12-09 10:55:06 -0500

    added splade models in train.sh

--- training-and-inference/train.sh
+++ training-and-inference/train.sh
@@ -190,9 +190,51 @@ case $TRAINER-${SETUP} in
     TOK_CKPOINT=math-tokenizer
     SHARDS_LIST=shards.txt
     TEST_FILE=test.txt
-    TEST_CYCLE=300
+    TEST_CYCLE=0 # 300
     CALL_ARGS="512" # qmax
     TRAINER_ARGS="--warmup-epochs 1 --lr 2e-5 --active_fp16"
+    ;;
+
+   single_vec_retriever-splade_all-a6000-using-bertnsp)
+    DEV_BSIZE=12
+    SAVE_FOLD=1
+
+    DATA_VER=yfEdE6sensBpCxT
+    START_POINT=models/job-pretrain-bertnsp-a6000-pretrain/6-0-0
+    TOK_CKPOINT=math-tokenizer
+    SHARDS_LIST=shards.txt
+    TEST_FILE=test.txt
+    TEST_CYCLE=0 # 300
+    CALL_ARGS=
+    TRAINER_ARGS="--architecture splade --splade_reg 1e-4 --splade_mask_mode all --warmup-epochs 1 --lr 2e-5"
+    ;;
+
+   single_vec_retriever-splade_somemath-a6000-using-bertnsp)
+    DEV_BSIZE=12
+    SAVE_FOLD=1
+
+    DATA_VER=yfEdE6sensBpCxT
+    START_POINT=models/job-pretrain-bertnsp-a6000-pretrain/6-0-0
+    TOK_CKPOINT=math-tokenizer
+    SHARDS_LIST=shards.txt
+    TEST_FILE=test.txt
+    TEST_CYCLE=0 # 300
+    CALL_ARGS=
+    TRAINER_ARGS="--architecture splade --splade_reg 1e-4 --splade_mask_mode somemath --warmup-epochs 1 --lr 2e-5"
+    ;;
+
+   single_vec_retriever-splade_nomath-a6000-using-bertnsp)
+    DEV_BSIZE=12
+    SAVE_FOLD=1
+
+    DATA_VER=yfEdE6sensBpCxT
+    START_POINT=models/job-pretrain-bertnsp-a6000-pretrain/6-0-0
+    TOK_CKPOINT=math-tokenizer
+    SHARDS_LIST=shards.txt
+    TEST_FILE=test.txt
+    TEST_CYCLE=0 # 300
+    CALL_ARGS=
+    TRAINER_ARGS="--architecture splade --splade_reg 1e-4 --splade_mask_mode nomath --warmup-epochs 1 --lr 2e-5"
     ;;
 
    *)
