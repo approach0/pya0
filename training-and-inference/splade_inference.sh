485d9668ec3bf22f8db9cdc5cfd30db09674d4f6
t-k- <clock126@126.com>
2022-12-10 16:13:07 -0500

    added training-and-inference/splade_inference.sh

---
+++ training-and-inference/splade_inference.sh
@@ -0,0 +1,22 @@
+set -e
+INFERENCE_PATH="$(pwd)"
+ANSERINI_PATH=$1
+CKPT="$(cd $2 && pwd)"
+TOPK=${3-1000}
+
+cd $ANSERINI_PATH
+sh ./target/appassembler/bin/IndexCollection -collection JsonVectorCollection \
+ -input $CKPT-doc \
+ -index $INFERENCE_PATH/indexes/$CKPT \
+ -generator DefaultLuceneDocumentGenerator -impact -pretokenized \
+ -threads 10
+
+tmpfile=$(mktemp)
+sh ./target/appassembler/bin/SearchCollection -hits $TOPK -parallelism 32 \
+ -index $INFERENCE_PATH/indexes/$CKPT \
+ -topicreader TsvInt -topics $CKPT-qry/output.tsv  \
+ -output $tmpfile -format trec \
+ -impact -pretokenized
+
+sed -e 's/^/A./g' $tmpfile > $INFERENCE_PATH/runs/$CKPT-top${TOPK}.run
+rm -f $tmpfile
