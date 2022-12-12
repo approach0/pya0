38fbcc19abdbfce3b9c82779536a6fb6a77dc583
t-k- <clock126@126.com>
2022-12-11 20:29:28 -0500

    update experiments/complement_matrix.sh to output more numbers.

--- experiments/complement_matrix.sh
+++ experiments/complement_matrix.sh
@@ -46,9 +46,6 @@ for i in "${!runlst[@]}"; do
         $eval_prefix/preprocess.sh cleanup
         $eval_prefix/preprocess.sh filter3_Dependency \
             $kfold_dir/*fold*train $kfold_dir/*fold*test
-        # rm some irrelevant results to speed up evaluation
-        rm -f $eval_prefix/input/*-Dependency-Text
-        find $eval_prefix/input/ -type f | grep -v 'Dependency' | xargs rm -f
         # evaluation
         $eval_prefix/eval.sh --nojudge
         cat $eval_prefix/result.tsv | sort | sed -e 's/[[:blank:]]/ /g' > kfold.tsv
@@ -57,10 +54,22 @@ for i in "${!runlst[@]}"; do
         dep_math__map=$($CV   --postfix='-Dependency-Formula' --score_field 2)
         dep_math__p_10=$($CV  --postfix='-Dependency-Formula' --score_field 3)
         dep_math__bpref=$($CV --postfix='-Dependency-Formula' --score_field 4)
+
         dep_both__ndcg=$($CV  --postfix='-Dependency-Both' --score_field 1)
         dep_both__map=$($CV   --postfix='-Dependency-Both' --score_field 2)
         dep_both__p_10=$($CV  --postfix='-Dependency-Both' --score_field 3)
         dep_both__bpref=$($CV --postfix='-Dependency-Both' --score_field 4)
-        echo "$name1 $name2 $dep_math__ndcg $dep_math__map $dep_math__p_10 $dep_math__bpref $dep_both__ndcg $dep_both__map $dep_both__p_10 $dep_both__bpref" >> kfold.result
+
+        dep_text__ndcg=$($CV  --postfix='-Dependency-Text' --score_field 1)
+        dep_text__map=$($CV   --postfix='-Dependency-Text' --score_field 2)
+        dep_text__p_10=$($CV  --postfix='-Dependency-Text' --score_field 3)
+        dep_text__bpref=$($CV --postfix='-Dependency-Text' --score_field 4)
+
+        all__ndcg=$($CV  --postfix='' --score_field 1)
+        all__map=$($CV   --postfix='' --score_field 2)
+        all__p_10=$($CV  --postfix='' --score_field 3)
+        all__bpref=$($CV --postfix='' --score_field 4)
+
+        echo "$name1 $name2 $dep_math__ndcg $dep_math__map $dep_math__p_10 $dep_math__bpref $dep_both__ndcg $dep_both__map $dep_both__p_10 $dep_both__bpref $dep_text__ndcg $dep_text__map $dep_text__p_10 $dep_text__bpref $all__ndcg $all__map $all__p_10 $all__bpref" >> kfold.result
     done
 done
