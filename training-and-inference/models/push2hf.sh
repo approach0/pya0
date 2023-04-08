c5231c9ad2e3a81e3dcf5d692792cf2ed1317191
t-k- <clock126@126.com>
2023-04-08 19:21:52 -0400

    update push2hf.sh: fully automatic.

--- training-and-inference/models/push2hf.sh
+++ training-and-inference/models/push2hf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #ssh -T git@hf.co
+huggingface-cli login
 
 #for bkb in cocomae bertnsp cocondenser cotmae; do
 #	read -p "create https://huggingface.co/approach0/backbone-$bkb-600" xxx
@@ -19,7 +20,7 @@ for bkb in cocomae bertnsp cocondenser cotmae cotbert vanilla-bert; do
     for ep in 0-2-0 1-2-0 2-2-0 3-2-0 5-2-0; do
         ep_short=$(echo $ep | sed -e 's/-//g')
         name=dpr-$bkb-$ep_short
-        read -p "Please create https://huggingface.co/approach0/ $name" xxx
+        python -c "from huggingface_hub import create_repo; create_repo('approach0/$name')"
         git clone git@github.com:approach0/azbert.git $name
         pushd $name
         cp -r ../../math-tokenizer ./ckpt
