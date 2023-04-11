2f701dce390a1166abd4cd8619b72a049509412a
t-k- <clock126@126.com>
2023-04-10 20:56:37 -0400

    utils/mindex_info.py: added arqmath-task2.

--- utils/mindex_info.py
+++ utils/mindex_info.py
@@ -10,11 +10,19 @@ MINDEX_INFO = {
         "image_filesystem": "reiserfs"
     },
     "arqmath-task1": {
-        "description": "ARQMath-3 corpus index",
+        "description": "ARQMath-3 Task-1 corpus index",
         "urls": [
             "https://vault.cs.uwaterloo.ca/s/c79mXwi8kkAMPSw/download"
         ],
         "md5": "24cd1b4a302ec3f5c0ae15508d3c79f7",
+        "image_filesystem": "reiserfs"
+    },
+    "arqmath-task2": {
+        "description": "ARQMath-3 Task-2 corpus index",
+        "urls": [
+            "https://vault.cs.uwaterloo.ca/s/zCtBtR7Y93fpBby/download"
+        ],
+        "md5": "f0256f6e7fa7b87bc855e4b10050ba91",
         "image_filesystem": "reiserfs"
     }
 }
